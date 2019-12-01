a2931fbc7de6395047901bb6d31dd2072ae80edf
andrzej <andrzej.szulc@gmail.com>
2019-12-01 21:38:40 +0100

    mvp - works for json & csv, quick & dirty

--- mvp.py
+++ mvp.py
@@ -2,16 +2,30 @@ import pathlib, json, csv
 import random
 from data import *
 
-file_dir = pathlib.Path("data")
-file_temp = "participants1.json"
-file_input = f"{file_dir}/{file_temp}"
 
-
-def open_file(filename, extension="json"):
+def open_file(filename, extension):
     if extension == "json":
-        with open(filename) as file_opened:
-            file_content = json.load(file_opened)
-        return file_content
+        with open(filename) as json_opened:
+            json_content = json.load(json_opened)
+        return json_content
+    elif extension == "csv":
+        with open(filename) as csv_opened:
+            csv_header = csv_opened.readlines(1)
+            csv_body = csv_opened.readlines()
+            csv_list = []
+            for e in csv_body:
+                line = str(str.split(e))
+                # line = str(line)
+                line_clean = line.replace("[","")
+                line_clean = line_clean.replace("]","")
+                line_clean = line_clean.replace("'","")
+                # print(line_clean)
+                # print(type(line))
+                # tupplee = tuple(line_clean)
+                # print(tupplee)
+                csv_list.append(line_clean)
+            return csv_list
+
 
 
 def check_file_extension(filename):
@@ -27,7 +41,7 @@ def draw_participants_json(json_content, how_many):
             participants_range = int(e["id"])
     winners_ids = random.sample(range(1, participants_range), how_many)
 
-    print(f"Zwycięzka {how_many} to:")
+    print(f"Zwycięska {how_many} to:")
     for g in json_content:
         if int(g["id"]) in winners_ids:
             winner_name = g["first_name"]
@@ -37,12 +51,37 @@ def draw_participants_json(json_content, how_many):
 
 
 def draw_participants_csv(csv_content, how_many):
-    pass
+    participants_range = 0
+    for e in csv_content:
+        first_coma_index = e.find(",")
+        participant_id = int(e[:first_coma_index])
+        if participant_id > participants_range:
+            participants_range = participant_id
+
+    winners_ids = random.sample(range(1, participants_range), how_many)
+    print(winners_ids)
+    #
+    print(f"Zwycięska {how_many} to:")
+    for g in csv_content:
+        first_coma_index = g.find(",")
+        participant_id = int(g[:first_coma_index])
+        if participant_id in winners_ids:
+            # winner_name = g["first_name"]
+            # winner_surname = g["last_name"]
+            winner = g[first_coma_index+1:]
+            winner_no_coma = winner.replace(",", " ")
+            print(winner_no_coma)
 
 
 
 if __name__ == "__main__":
-    file_content = open_file(file_input)
+    file_dir = pathlib.Path("data")
+    file_temp = "participants1.csv"
+    file_input = f"{file_dir}/{file_temp}"
+
+    file_extension = check_file_extension(file_temp)
+    file_content = open_file(file_input, file_extension)
     # print(file_content)
     # print(check_file_extension(file_input))
-    draw_participants_json(file_content, 3)
+    # draw_participants_json(file_content, 3)
+    draw_participants_csv(file_content, 3)
