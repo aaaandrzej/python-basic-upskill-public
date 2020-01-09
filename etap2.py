e882f1dc661ad51533f2e5d4fbd5a990e2ca9e0a
andrzej <andrzej.szulc@gmail.com>
2020-01-09 19:21:56 +0100

    etap2 - export winners and prizes to json

--- etap2.py
+++ etap2.py
@@ -1,4 +1,5 @@
 from mvp import *
+import json
 
 
 class Prize:
@@ -12,14 +13,15 @@ class Prize:
 
 
 class Winner(Participant):
-    def __init__(self, id, first_name, last_name, prize):
-        self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self, name, prize):
+        self.name = name
         self.prize = prize
 
     def __repr__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.name} - {self.prize}"
+
+    def to_dict(self):
+        return {"name":self.name, "prize":self.prize}
 
 
 def extract_lottery_scheme(filename):
@@ -32,30 +34,39 @@ def extract_lottery_scheme(filename):
         return lottery_prizes_list
 
 
-def draw_participants(participants_list, how_many, prizes):
+def draw_participants(participants_list, how_many, prizes, file_output):
     winners_list = random.sample(participants_list, k=how_many)
+    winners_with_prizes = []
+    winners_with_prizes_to_json = []
 
-    print(f"Zwycięska {how_many} to:")
+
     for winner in winners_list:
         try:
             indexxx = winners_list.index(winner)
             prize = prizes[indexxx]
         except IndexError:
             prize = "Audience Prize"
-        winner_ = f"{winner} - {prize}"
-        print(winner_)
+        winners_with_prizes.append(Winner(winner, prize))
+
+    print(f"Zwycięska {how_many} to:")
+    for winner_prize in winners_with_prizes:
+        print(winner_prize)
+        winners_with_prizes_to_json.append(winner_prize.to_dict())
+
+    with open(file_output, 'w', encoding='utf-8') as file:
+        file.write(json.dumps(str(winners_with_prizes_to_json)))
 
 
 if __name__ == "__main__":
     file_dir = pathlib.Path("data")
     file_temp = "participants2.json"
     file_input = f"{file_dir}/{file_temp}"
+    file_output = f"{file_dir}/result.json"
 
     how_many_winners = 4
 
-    # lottery_scheme = "data/lottery_templates/separate_prizes.json"
-    lottery_scheme = "data/lottery_templates/item_giveaway.json"
-
+    lottery_scheme = "data/lottery_templates/separate_prizes.json"
+    # lottery_scheme = "data/lottery_templates/item_giveaway.json"
 
     file_extension = check_file_extension(file_temp)
     file_content = open_file(file_input, file_extension)
@@ -64,4 +75,4 @@ if __name__ == "__main__":
 
     prizes = extract_lottery_scheme(lottery_scheme)
 
-    draw_participants(file_content, how_many_winners, prizes)
+    draw_participants(file_content, how_many_winners, prizes, file_output)
