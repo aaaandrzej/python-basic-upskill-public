1afd710d97b4741807750e338282e14e13048d62
andrzej <andrzej.szulc@gmail.com>
2020-02-01 23:47:06 +0100

    refactor of lottery.py + basic unittests in test_lottery.py

---
+++ test_lottery.py
@@ -0,0 +1,31 @@
+import unittest
+from lottery import Participant, Prize, Winner
+
+
+class TestClasses(unittest.TestCase):
+    def test_participant(self):
+        p = Participant(1, "Jan", "Nowak", 2)
+        self.assertEqual(p.id, 1)
+        self.assertEqual(p.first_name, "Jan")
+        self.assertEqual(p.last_name, "Nowak")
+        self.assertEqual(p.weight, 2)
+
+    def test_prize(self):
+        p = Prize(1, "Jan Nowak", 2)
+        self.assertEqual(p.id, 1)
+        self.assertEqual(p.name, "Jan Nowak")
+        self.assertEqual(p.amount, 2)
+
+
+    def test_winner(self):
+        w = Winner("Jan Nowak", "Gold medal")
+        self.assertEqual(w.name, "Jan Nowak")
+        self.assertEqual(w.prize, "Gold medal")
+
+    def test_winner_to_dict(self):
+        w = Winner("Jan Nowak", "Gold medal")
+        self.assertEqual(w.to_dict(), {"name": "Jan Nowak", "prize": "Gold medal"})
+
+
+if __name__ == '__main__':
+    unittest.main()
