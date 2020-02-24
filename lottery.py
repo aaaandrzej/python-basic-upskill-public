4ff2a1048058c8b19e70b968736cf03aef2e3c0e
andrzej <andrzej.szulc@gmail.com>
2020-02-24 17:25:41 +0100

    have slit logic into 4 py files instead of just 1, each file contains their class and relevant functions now

--- lottery.py
+++ lottery.py
@@ -24,41 +24,12 @@ def main(file_input, file_extension, scheme, file_output):
     winners_with_prizes = draw_winners(file_content, prizes)
 
     write_winners_to_json(winners_with_prizes, file_output)
-
     print_winners(winners_with_prizes)
 
     summary = f"(wylosowano z {file_input} z użyciem {scheme} i zapisano do {file_output})"
-
     print(summary)
 
 
 if __name__ == "__main__":
 
     main()  # file_input argument required, e.g.: "python lottery.py participants1.json"
-
-'''
-# scripts for testing only - commented by default
-    file_input="data/participants2.json"
-    file_extension="json"
-    scheme="data/lottery_templates/item_giveaway.json"
-    file_output="data/result.json"
-
-    if file_extension == "json":
-        file_content = extract_participants_from_json(file_input)
-    elif file_extension == "csv":
-        file_content = extract_participants_from_csv(file_input)
-
-    prizes = extract_prizes_from_scheme(scheme)
-
-    winners_with_prizes = draw_winners(file_content, prizes)
-
-    # write_winners_to_json(winners_with_prizes, file_output)
-    #
-    # print_winners(winners_with_prizes)
-    #
-    # summary = f"(wylosowano z {file_input} z użyciem {scheme} i zapisano do {file_output})"
-    #
-    # print(summary)
-
-    print(winners_with_prizes)
-'''
